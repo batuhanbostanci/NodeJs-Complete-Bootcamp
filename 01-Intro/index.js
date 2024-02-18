c286e9c1f6ea26bd780bf3ec92eed6341414a4bc
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-02-18 12:20:43 +0300

    Very basic changes

--- 01-Intro/index.js
+++ 01-Intro/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+/////////////////////////////////////////
 //Blocking Synchronous way
 
 // const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -10,7 +13,35 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/out.txt", textOut);
 
 //Non-Blocking Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  if (err) return console.log("Error");
-  console.log(data);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   if (err) return console.log("Error");
+//   console.log(data);
+// });
+
+//Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    console.log("This is the overview");
+  } else if (pathName === "/product") {
+    console.log("This is the product");
+  } else if (pathName === "/api") {
+    console.log("This is the API");
+
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
 });
