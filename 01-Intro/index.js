28d0d1353a4210119dd9d52757d538ff02bb76bd
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-03-22 12:30:21 +0300

    Creating Basic Server

--- 01-Intro/index.js
+++ 01-Intro/index.js
@@ -1,16 +1,26 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-//Blocking Synchronous way
+// //Blocking Synchronous way
 
-// const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+// // const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 
-// console.log(textInput);
+// // console.log(textInput);
 
-// const textOut = `This is what we know about the avakado ${textInput}\n Created at ${Date.now()}`;
-// fs.writeFileSync("./txt/out.txt", textOut);
+// // const textOut = `This is what we know about the avakado ${textInput}\n Created at ${Date.now()}`;
+// // fs.writeFileSync("./txt/out.txt", textOut);
 
-//Non-Blocking Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  if (err) return console.log("Error");
-  console.log(data);
+// //Non-Blocking Asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   if (err) return console.log("Error");
+//   console.log(data);
+// });
+
+const http = require("http");
+const url = require("url");
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen(8000, "localhost", () => {
+  console.log("Server is listening to the port 8000");
 });
