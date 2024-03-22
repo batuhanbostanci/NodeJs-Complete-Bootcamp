07b04a63cd99afc5c89486f6ceb527b22e1c49a3
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-03-22 12:38:56 +0300

    Breaking the section 1

--- 01-Intro/index.js
+++ 01-Intro/index.js
@@ -15,12 +15,34 @@
 //   console.log(data);
 // });
 
+// const http = require("http");
+// const url = require("url");
+// const server = http.createServer((req, res) => {
+//   res.end("Hello from the server");
+// });
+
+// server.listen(8000, "localhost", () => {
+//   console.log("Server is listening to the port 8000");
+// });
+
 const http = require("http");
 const url = require("url");
-const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
-});
 
-server.listen(8000, "localhost", () => {
-  console.log("Server is listening to the port 8000");
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.end("API");
+  } else {
+    res.write(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+
+    res.end("<h1>Page not found </h1>");
+  }
 });
