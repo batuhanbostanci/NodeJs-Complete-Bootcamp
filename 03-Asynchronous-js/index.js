dd293032c9ea1b680bf2ca521fa441e06adbba28
batuhanbostanci <batuhanbostanci2@hotmail.com>
2024-04-01 12:21:08 +0300

    Adding promises part

--- 03-Asynchronous-js/index.js
+++ 03-Asynchronous-js/index.js
@@ -1,14 +1,60 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+
+//         console.log('image was saved successfully');
+//       });
+//     });
+// });
+
+//Starting promises part
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+
+//         console.log('image was saved successfully');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
+
+const readFilePromises = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('i could not find that file');
+      resolve(data);
+    });
+  });
+};
+
+readFilePromises(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
@@ -16,5 +62,8 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
         console.log('image was saved successfully');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
